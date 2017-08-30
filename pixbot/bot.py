82903eacd12c4e3d00c908068b98f59c46d78e30
j.c.sackett <j.c.sackett@gmail.com>
2017-08-30 20:51:20 +0000

    Adds debug and fixes start command

--- pixbot/bot.py
+++ pixbot/bot.py
@@ -1,3 +1,5 @@
+# TODO add webserver that has a get that takes secret key and then adds job to send
+
 import os
 import json
 import threading
@@ -11,38 +13,45 @@ from telegram.ext import (
 KEY = os.environ.get('PIXBOT_TELEGRAM_KEY')
 USER_STORE = os.environ.get('PIXBOT_STORE', os.path.join(os.environ.get('PWD'), '.pixuserstore'))
 ADMIN_USER = os.environ.get('PIXBOT_ADMIN')
+DEBUG = True if os.environ.get('PIXBOT_DEBUG') == 'True' else False
+
 
 updater = Updater(token=KEY)
+
 
 def _shutdown():
     updater.stop()
     updater.is_idle = False
 
-# TODO add register command that registers a secret key with the user
-# TODO add webserver that has a get that takes secret key and then adds job to send
-# pic to chat_id corresponding to that secret key
+
 def start(bot, update):
     chat_id = update.message.chat_id
     username = update.message.from_user.name
     try:
         store = json.load(file(USER_STORE))
-    except ValueError:
+        if DEBUG:
+            print 'Found store at %s' % USER_STORE
+    except IOError, ValueError:
+        if DEBUG:
+            print 'Generating new store'
         store = {}
     store[username] = chat_id
-    json.dump(file(USER_STORE, 'w'), store)
+    json.dump(store, file(USER_STORE, 'w'))
     bot.send_message(
         chat_id=chat_id,
         text='Hi there! People can now use me to send you pics!')
 
+
 def shutdown(bot, update):
     username = update.message.from_user.name
+    if DEBUG:
+        print username
     if username == ADMIN_USER:
+        if DEBUG:
+            print "shutting down..."
         threading.Thread(target=_shutdown).start()
-
-
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('shutdown', shutdown))
-
 updater.start_polling()
